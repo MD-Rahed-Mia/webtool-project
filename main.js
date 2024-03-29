60124fe1ec8235c811cf595cddbf35680020843a
Md Rahed Mia <135752931+MD-Rahed-Mia@users.noreply.github.com>
2024-03-29 12:56:01 +0600

    dark-mode light mode added

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ const menuBtn = document.querySelector(".navbar-menu-icon");
 menuBtn.addEventListener("click", () => {
   menuContainer.classList.toggle("active");
 });
+
+
+
+const themeBtn = document.querySelector(".theme-icon");
+themeBtn.addEventListener("click", () => {
+  let themeAttr = document.body.getAttribute("data-theme");
+  themeAttr == "light"
+    ? document.body.setAttribute("data-theme", "dark")
+    : document.body.setAttribute("data-theme", "light");
+});
