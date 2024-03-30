5905d77ab950ef4fcdca7686dda17690f85ce65a
Md Rahed Mia <135752931+MD-Rahed-Mia@users.noreply.github.com>
2024-03-30 22:29:17 +0600

    hjh

--- public/tool_functionality/userIp.js
+++ public/tool_functionality/userIp.js
@@ -1,8 +1,10 @@
 const IpText = document.querySelector("#ip-text");
 
+console.log(window.location);
+
 async function checkWebsiteStatus() {
   try {
-    const apiData = await fetch("http://localhost:3000/getip", {
+    const apiData = await fetch(`${window.location.host}/getip`, {
       mode: "no-cors",
     });
     const data = await apiData.text();
